6eff50b459499fa7e0c40032346b2a97b2b21e3a
Jochen Kirstätter <7329802+jochenkirstaetter@users.noreply.github.com>
2025-02-19 09:56:37 +0400

    Update code_execution.sh
    Consistent of array for `contents` and `parts` elements.

--- samples/rest/code_execution.sh
+++ samples/rest/code_execution.sh
@@ -5,12 +5,11 @@ echo "[START code_execution_basic]"
 curl "https://generativelanguage.googleapis.com/v1beta/models/gemini-1.5-flash:generateContent?key=$GOOGLE_API_KEY" \
 -H 'Content-Type: application/json' \
 -d ' {"tools": [{"code_execution": {}}],
-    "contents": {
-      "parts": 
-        {
-            "text": "What is the sum of the first 50 prime numbers? Generate and run code for the calculation, and make sure you get all 50."
-        }
-    },
+    "contents": [{
+      "parts": [{
+        "text": "What is the sum of the first 50 prime numbers? Generate and run code for the calculation, and make sure you get all 50."
+      }]
+    }],
 }'
 # [END code_execution_basic]
 
